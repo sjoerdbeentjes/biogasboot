c5bf8940e19010a137149d684a05f865afebc75c
Sjoerdbeentjes <mail@sjoerdbeentjes.nl>
2017-05-29 16:24:55 +0200

    base for login

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', ensureAuthenticated, (req, res, next) => {
   res.render('index', {title: 'Express'});
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/users/login');
+  }
+}
 
 module.exports = router;
