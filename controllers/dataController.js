4d005faaa99822c21b80271b38bbbe8f8f4cc053
Diego Staphorst <diego-s-@hotmail.com>
2017-05-30 14:37:33 +0200

    Add linear Regression function

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,7 +1,7 @@
 const lineByLine = require('line-by-line');
 const file = new lineByLine('../data/data/worked-data.csv');
 
-let arrayToDetermineDirection = [];
+let yValues = [];
 
 file.on('error', function (err) {
     // 'err' contains error object
@@ -14,8 +14,9 @@ file.on('line', function (line) {
     // ...do your asynchronous line processing..
     setTimeout(function () {
         let data = line.split(',');
-        let averageTemperature = (parseInt(data[2]) + parseInt(data[3])) / 2;
-        arrayToDetermineDirection.push(averageTemperature);
+        let averageTemperature = (parseFloat(data[2]) + parseFloat(data[3])) / 2;
+        yValues.push(averageTemperature.toFixed(2));
+        if (yValues.length > 5) yValues.shift();
         calculateDirection();
         file.resume();
     }, 100);
@@ -24,3 +25,35 @@ file.on('line', function (line) {
 file.on('end', function () {
     // All lines are read, file is closed now.
 });
+
+function calculateDirection() {
+    let direction;
+    let xValues = Array.apply(null, {length: yValues.length}).map(Number.call, Number);
+    let regression = linearRegression(yValues, xValues);
+    console.log(regression);
+}
+// http://www.localwisdom.com/blog/2014/01/get-trend-line-javascript-graph-linear-regression/
+function linearRegression(y, x) {
+    var lr = {};
+    var n = y.length;
+    var sum_x = 0;
+    var sum_y = 0;
+    var sum_xy = 0;
+    var sum_xx = 0;
+    var sum_yy = 0;
+
+    for (var i = 0; i < y.length; i++) {
+
+        sum_x += x[i];
+        sum_y += y[i];
+        sum_xy += (x[i] * y[i]);
+        sum_xx += (x[i] * x[i]);
+        sum_yy += (y[i] * y[i]);
+    }
+
+    lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
+    lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
+    lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
+
+    return lr;
+}
