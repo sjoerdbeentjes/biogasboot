6c00aa9ae1aec75652fee463b081c5cd35a74a63
Sjoerdbeentjes <mail@sjoerdbeentjes.nl>
2017-05-30 17:30:00 +0200

    base for styles

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,1 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
-
-var hoi = 'Hallo';
-var hoi2 = '1';
-
-console.log(hoi);
-
-},{}]},{},[1]);
+console.error("Error: Couldn't find preset \"es2015\" relative to directory \"/Users/Sjoerd/Desktop/CMD v2/minor/biogasboot/src/js\" while parsing file: /Users/Sjoerd/Desktop/CMD v2/minor/biogasboot/src/js/index.js");
