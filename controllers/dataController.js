d350b99c504733dfca0ee0a4c11df92cb6571324
Diego Staphorst <diego-s-@hotmail.com>
2017-05-30 15:50:49 +0200

    Notify when radiator is on

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,7 +1,6 @@
 const lineByLine = require('line-by-line');
 const file = new lineByLine('../data/data/worked-data.csv');
-
-let yValues = [];
+let heaterStatus;
 
 file.on('error', function (err) {
     // 'err' contains error object
@@ -14,46 +13,18 @@ file.on('line', function (line) {
     // ...do your asynchronous line processing..
     setTimeout(function () {
         let data = line.split(',');
-        let averageTemperature = (parseFloat(data[2]) + parseFloat(data[3])) / 2;
-        yValues.push(averageTemperature.toFixed(2));
-        if (yValues.length > 5) yValues.shift();
-        calculateDirection();
+        let averageTemp = (parseFloat(data[2]) + parseFloat(data[3]))/2;
+
+        if (averageTemp <= 34) {heaterStatus = true};
+        if (heaterStatus && averageTemp >= 36.7) {heaterStatus = false};
+
+        if (heaterStatus) console.log("verwarming is aan")
+        else console.log("verwarming is uit")
+
         file.resume();
-    }, 100);
+    }, 1);
 });
 
 file.on('end', function () {
     // All lines are read, file is closed now.
 });
-
-function calculateDirection() {
-    let direction;
-    let xValues = Array.apply(null, {length: yValues.length}).map(Number.call, Number);
-    let regression = linearRegression(yValues, xValues);
-    console.log(regression);
-}
-// http://www.localwisdom.com/blog/2014/01/get-trend-line-javascript-graph-linear-regression/
-function linearRegression(y, x) {
-    var lr = {};
-    var n = y.length;
-    var sum_x = 0;
-    var sum_y = 0;
-    var sum_xy = 0;
-    var sum_xx = 0;
-    var sum_yy = 0;
-
-    for (var i = 0; i < y.length; i++) {
-
-        sum_x += x[i];
-        sum_y += y[i];
-        sum_xy += (x[i] * y[i]);
-        sum_xx += (x[i] * x[i]);
-        sum_yy += (y[i] * y[i]);
-    }
-
-    lr['slope'] = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
-    lr['intercept'] = (sum_y - lr.slope * sum_x) / n;
-    lr['r2'] = Math.pow((n * sum_xy - sum_x * sum_y) / Math.sqrt((n * sum_xx - sum_x * sum_x) * (n * sum_yy - sum_y * sum_y)), 2);
-
-    return lr;
-}
