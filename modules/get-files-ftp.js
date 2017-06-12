6e7d04ba8ec2dd8557ccb400281d06c56c5acea8
Timo Verkroost <timo.verkroost@gmail.com>
2017-06-12 13:11:50 +0200

    Updated FTP download of files from external server. Sorted names and timestamps

--- modules/get-files-ftp.js
+++ modules/get-files-ftp.js
@@ -4,20 +4,21 @@ require('dotenv').config();
 
 function getFTPfiles() {
   // Setup  connections to external FTP server
+  // Setup for VALUE
   const ftpValue = new JSFtp({
     host: process.env.FTP_SERVER,
     port: 21,
     user: process.env.FTP_USER,
     pass: process.env.FTP_PASS
   });
-
+  // Setup for STATUS
   const ftpStatus = new JSFtp({
     host: process.env.FTP_SERVER,
     port: 21,
     user: process.env.FTP_USER,
     pass: process.env.FTP_PASS
   });
-
+  // Setup for ALARM
   const ftpAlarm = new JSFtp({
     host: process.env.FTP_SERVER,
     port: 21,
@@ -27,35 +28,69 @@ function getFTPfiles() {
 
   // Gets latest file from FTP server and place it on this server (every 30 min, 1800 seconds)
   setInterval(() => {
+    console.log('-----New update from FTP server-----');
     // Update VALUE dir
     ftpValue.ls("/uploads/VALUE/VALUE/", function(err, res) {
-      // Get latest file name
-      const totalFilesVALUE = res.length - 1;
-      const latestFileVALUE = res[totalFilesVALUE].name;
-      console.log(res[totalFilesVALUE]);
-      // Copy latest from FTP to this server
+
+      // Sort by filename
+      const byName = res.slice(0);
+      byName.sort(function(a,b) {
+        return a.name - b.name;
+      });
+
+      // Get latest file name (by name)
+      const totalFilesVALUE = byName.length - 1;
+      const latestFileVALUE = byName[totalFilesVALUE].name;
+        // Copy latest from FTP to this server
       ftpValue.get('/uploads/VALUE/VALUE/'+ latestFileVALUE, path.join(__dirname, `../data/ftp/VALUE/${latestFileVALUE}`), function(hadErr) {
         if (hadErr)
           console.error(hadErr);
         else
-          console.log('VALUE file copied successfully!');
+          console.log('VALUE file copied successfully! Filename: ' + latestFileVALUE);
       });
     });
 
     // Update STATUS dir
     ftpStatus.ls("/uploads/STATUS/STATUS/", function(err, res) {
-      // Get latest file name
-      const totalFilesSTATUS = res.length - 1;
-      const latestFileSTATUS = res[totalFilesSTATUS].name;
+
+      // Sort by filename
+      const byName = res.slice(0);
+      byName.sort(function(a,b) {
+        return a.name - b.name;
+      });
+
+      // Get latest file name (by name)
+      const totalFilesSTATUS = byName.length - 1;
+      const latestFileSTATUS = byName[totalFilesSTATUS].name;
       // Copy latest from FTP to this server
       ftpStatus.get('/uploads/STATUS/STATUS/'+ latestFileSTATUS, path.join(__dirname, `../data/ftp/STATUS/${latestFileSTATUS}`), function(hadErr) {
         if (hadErr)
           console.error(hadErr);
         else
-          console.log('STATUS file copied successfully!');
+          console.log('STATUS file copied successfully! Filename: ' + latestFileSTATUS);
       });
     });
-  }, 10000000);
+
+    // // Update ALARM dir
+    ftpAlarm.ls("/uploads/ALARM/", function(err, res) {
+      // Sort by timestamp
+      const byDate = res.slice(0);
+      byDate.sort(function(a,b) {
+        return a.time - b.time;
+      });
+
+      // Get latest file name
+      const totalFilesALARM = byDate.length - 1;
+      const latestFileALARM = byDate[totalFilesALARM].name;
+      // Copy latest from FTP to this server
+      ftpAlarm.get('/uploads/ALARM/'+ latestFileALARM, path.join(__dirname, `../data/ftp/ALARM/${latestFileALARM}`), function(hadErr) {
+        if (hadErr)
+          console.error(hadErr);
+        else
+          console.log('ALARM file copied successfully! Filename: ' + latestFileALARM);
+      });
+    });
+  }, 1800000);
 
 }
 
