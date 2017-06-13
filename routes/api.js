57d735daf00d3feb99309de3fe37a6242db63ecb
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-06-13 11:09:43 +0200

    Create API endpoints for requesting history data #18 Possible to get average data from one specific date

--- routes/api.js
+++ routes/api.js
@@ -31,6 +31,28 @@ function setData(req, res) {
   });
 }
 
+function filterData(format, date, data) {
+  const returnData = [];
+  const dataObject = {Date: new Date(Number(date) * 1000), 'Temp_PT100_1': 0, 'Temp_PT100_2': 0, pH_Value: 0, Bag_Height: 0, count: 0};
+  const parsedDate = parseDate(new Date(Number(date) * 1000));
+  data.forEach(function(point) {
+    let thisDate = parseDate(point.Date);
+    if(thisDate == parsedDate) {
+      dataObject['Temp_PT100_1'] = dataObject['Temp_PT100_1'] + Number(point['Temp_PT100_1']);
+      dataObject['Temp_PT100_2'] = dataObject['Temp_PT100_2'] + Number(point['Temp_PT100_2']);
+      dataObject['pH_Value'] = dataObject['pH_Value'] + Number(point['pH_Value']);
+      dataObject['Bag_Height'] = dataObject['Bag_Height'] + Number(point['Bag_Height']);
+      dataObject['count'] = dataObject['count'] +  1;
+    }
+  });
+  returnData.push(dataObject);
+  return returnData;
+}
+
+function parseDate(date) {
+  return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+}
+
 /* GET home page. */
 router.get('/all', (req, res, next) => {
   // Check for parameters and use them for serving data
@@ -53,7 +75,12 @@ router.get('/all', (req, res, next) => {
     // setData(req, res);
     // console.log(DataPoint)
     DataPoint.find((err, data) => {
-      res.send(data);
+      if(req.param('format') && req.param('date')) {
+        const formattedData = filterData(req.param('format'), req.param('date'), data);
+        res.send(formattedData);
+      } else {
+        res.send(data);
+      }
     });
   }
 });
