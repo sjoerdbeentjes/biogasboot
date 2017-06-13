7838704d2d5da59686da7b2c6e3848b0b3bcd7f1
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-06-13 11:47:47 +0200

    Setup for the customer dashboard

--- routes/customer-dashboard.js
+++ routes/customer-dashboard.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('customer-dashboard', {title: 'Express'});
+  res.render('dashboard/index', {title: 'Express'});
 });
 
 module.exports = router;
