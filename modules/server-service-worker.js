c91da15157ea6d866d1f515620006dbfd437b92f
Timo Verkroost <timo.verkroost@gmail.com>
2017-06-14 19:04:40 +0200

    Added subscriptions to DB + add and remove them

--- modules/server-service-worker.js
+++ modules/server-service-worker.js
@@ -1,10 +1,12 @@
 const webPush = require('web-push');
 require('dotenv').config();
 
+const Subscription = require('../models/subscription');
+
 function serviceWorker(app) {
 
 // Store subscriptions
-  const subscriptions = [];
+  //const subscriptions = [];
   const payload = 'TESTEN';
 
 // Push interval (10 seconds)
@@ -36,67 +38,30 @@ function serviceWorker(app) {
   }
 
 //Simulate send notifications needs to be disabled
-  setInterval(() => {
-    subscriptions.forEach(sendNotification);
-  }, pushInterval * 1000);
+//   setInterval(() => {
+//     subscriptions.forEach(sendNotification);
+//   }, pushInterval * 1000);
 
-  function isSubscribed(endpoint) {
-    // let i;
-    // for( i = subscriptions.length; i >= 0; i++) {
-    //   if( subscriptions[i].endpoint === endpoint){
-    //     return console.log(subscriptions[i]);
-    //     //return subscriptions[i].endpoint;
-    //   }
-    // }
-    // return (Object.keys(subscriptions).indexOf(endpoint) >= 0);
-    //return (subscriptions.indexOf(endpoint) >= 0);
-    // for (let key in subscriptions) {
-    //   let value = subscriptions[key].endpoint;
-    //   return (value.indexOf(endpoint) >= 0);
-    // }
-    // const result = subscriptions.filter(function( obj ) {
-    //   return obj.endpoint === endpoint;
-    // });
-    // return result;
-    // const indexes = subscriptions.map(function(obj, index) {
-    //   if(obj.endpoint === endpoint) {
-    //     return index;
-    //   }
-    // }).filter(isFinite);
-    // return (indexes >= 0);
-    // const index = subscriptions.findIndex(endpoint);
-    // return index;
-    // let check = subscriptions.filter(function( obj, idx ) {
-    //   if( obj.endpoint === endpoint ) {
-    //     const index = idx;
-    //     return true;
-    //   } else {
-    //     return false
-    //   }
-    // });
-    // return check;
-    // for (var i = 0; i < subscriptions.length - 1; i++) {
-    //
-    //     if (subscriptions[i].endpoint === endpoint) {
-    //       return true;
-    //     }
-    // }
-    //   return false
-    const index = subscriptions.findIndex(x => x.endpoint==endpoint);
-    return index;
-
-    }
-
-    //return (subscriptions.indexOf(endpoint) >= 0);
+  // Check if device is already subscribed
+  function checkSubscription(endpoint) {
+    return Subscription.find({endpoint: endpoint}, function (err, docs) {
+      return docs;
+    });
+  }
 
 // Register a subscription by adding an endpoint to the `subscriptions`
   app.post('/operator/register-serviceworker', (req, res) => {
+    // Get parameters to save device
     const endpoint = req.body.endpoint;
     const p256dh = req.body.key;
     const auth = req.body.authSecret;
-    console.log('r: ' + isSubscribed(endpoint));
-    if (!isSubscribed(endpoint)) {
-      subscriptions.push( [{endpoint: endpoint, p256dh:p256dh, auth:auth}]);
+    if (checkSubscription(endpoint)) {
+      console.log('added');
+      // Add subscription to DB
+      const newSubscription = new Subscription({endpoint: endpoint, p256dh: p256dh, auth: auth});
+      newSubscription.save(function (err) {
+        if (err) return console.error(err);
+      });
     }
     res.type('js').send('{"success":true}');
   });
@@ -104,20 +69,10 @@ function serviceWorker(app) {
   // Unregister a subscription by removing it from the `subscriptions` array
   app.post('/operator/unregister-serviceworker', (req, res) => {
     const endpoint = req.body.endpoint;
-    //console.log(subscriptions);
-    console.log('u: ' + isSubscribed(endpoint));
-    if (isSubscribed(endpoint)) {
-      //console.log(endpoint);
-      console.log('Subscription unregistered ' + endpoint);
-      //subscriptions.splice(subscriptions.findIndex(x => x.endpoint === endpoint), 1);
-      // let i;
-      // for( i = subscriptions.length; i>=0; i--) {
-      //   if( subscriptions[i].endpoint === endpoint){
-      //     subscriptions.splice(i,1);
-      //     console.log('test: ' + subscriptions);
-      //   }
-      // }
-      // console.log(subscriptions);
+    if (checkSubscription(endpoint)) {
+      console.log('removed');
+      // Remove device from DB based in endpoint
+      Subscription.findOneAndRemove({endpoint: endpoint}, function (err, docs) {});
     }
     res.type('js').send('{"success":true}');
   });
