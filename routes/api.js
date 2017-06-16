c166e7b5c09969f413c2903bc1029ea9b3d71773
Sjoerdbeentjes <mail@sjoerdbeentjes.nl>
2017-06-16 12:28:36 +0200

    new endpoints for API

--- routes/api.js
+++ routes/api.js
@@ -128,4 +128,173 @@ router.get('/all', (req, res, next) => {
   }
 });
 
+router.get('/all', (req, res, next) => {
+  // Check for parameters and use them for serving data
+  // console.log(req.param('format'))
+  if (req.param('dateStart') && req.param('dateEnd')) {
+    let filteredData = [];
+    let averageObject = {};
+    DataPoint.find((err, data) => {
+      data.forEach(function(point) {
+        const thisDate = new Date(point.Date);
+        const startDate = new Date(Number(req.param('dateStart')) * 1000);
+        const endDate = new Date(req.param('dateEnd') * 1000);
+        // Check if the actual date is in between the start- & end date
+        if (thisDate >= startDate && thisDate <= endDate) {
+          // Check if the data has to be formatted
+          if (req.param('format')) {
+            const format = req.param('format');
+            const date = parseDate(point.Date);
+            // Check if the actual date has a instance in the averageObject allready
+            if (averageObject[date]) {
+              // Update de averageObject data with the actual data
+              averageObject[date] = {
+                Date: averageObject[date]['Date'],
+                'Temp_PT100_1': Number(point['Temp_PT100_1']) + averageObject[date]['Temp_PT100_1'],
+                'Temp_PT100_2': Number(point['Temp_PT100_2']) + averageObject[date]['Temp_PT100_2'],
+                pH_Value: Number(point['pH_Value']) + averageObject[date]['pH_Value'],
+                Bag_Height: Number(point['Bag_Height']) + averageObject[date]['Bag_Height'],
+                count: + averageObject[date]['count'] + 1
+              };
+            } else {
+              // If there is no instance in the averageObject yet, create one
+              averageObject[date] = {
+                Date: point.Date,
+                'Temp_PT100_1': Number(point['Temp_PT100_1']),
+                'Temp_PT100_2': Number(point['Temp_PT100_2']),
+                pH_Value: Number(point['pH_Value']),
+                Bag_Height: Number(point['Bag_Height']),
+                count: 1
+              };
+            }
+          } else {
+            // Push the filtered data if there is no format option declared
+            filteredData.push(point)
+          }
+        };
+      });
+      // Loop trough average values in averageObject and push to the filteredData array
+      for(key in averageObject) {
+        filteredData.push(averageObject[key])
+      }
+      res.send(filteredData)
+    });
+  } else {
+    // uncomment this function if the sample data needs to be reset (first delete collection in database)
+    // setData(req, res);
+    // console.log(DataPoint)
+    DataPoint.find((err, data) => {
+      if (req.param('format') && req.param('date')) {
+        const formattedData = filterData(req.param('format'), req.param('date'), data);
+        res.send(formattedData);
+      } else {
+        res.send(data);
+      }
+    });
+  }
+});
+
+/* GET home page. */
+router.get('/latest', (req, res, next) => {
+  DataPoint.findOne({}, {}, {sort: {Date: -1}}, (err, point) => {
+    res.send(point);
+  });
+});
+
+/* GET home page. */
+router.get('/status', (req, res, next) => {
+  DataPoint.findOne({}, {}, {sort: {Date: -1}}, (err, point) => {
+    res.send(tileSatus(point));
+  });
+});
+
+function tileSatus(data) {
+  // values when attention is required or not.
+  const statusData = {
+    phStatus: 0,
+    tempStatus: 0,
+    gasbagStatus: 0
+  };
+  // Define low and high values for indicator, optional: min and max
+  const types = {
+    ph: {
+      low: 7,
+      high: 7.5,
+      min: 1,
+      max: 14
+    },
+    temp: {
+      low: 34.6,
+      high: 38
+    },
+    gasbag: {
+      low: 145,
+      high: 160,
+      min: 10,
+      max: 200
+    }
+  };
+  // Explain number meanings
+  // 0 = Good
+  // 1 = Error
+  // Gasbag indicator
+  switch (true) {
+    case data.Gaszak_hoogte_hu >= types.gasbag.low && data.Gaszak_hoogte_hu <= types.gasbag.high:
+      // Good
+      statusData.gasbagStatus = 0;
+      break;
+    case data.Gaszak_hoogte_hu > types.gasbag.high:
+      // ADD NOTIFICATION FUNCTION HERE
+      // Error + bag is almost full
+      statusData.gasbagStatus = 1;
+      break;
+    case data.Gaszak_hoogte_hu < types.gasbag.low:
+      // ADD NOTIFICATION FUNCTION HERE
+      // Error + bag is almost empty
+      statusData.gasbagStatus = 1;
+      break;
+    default:
+      statusData.gasbagStatus = 0;
+      break;
+  }
+  // PH indicator
+  switch (true) {
+    case data.ph_value >= types.ph.low && data.ph_value <= types.ph.high:
+      // Good
+      statusData.phStatus = 0;
+      break;
+    case data.ph_value > types.ph.high:
+      // Error + ph to high
+      statusData.phStatus = 1;
+      break;
+    case data.ph_value < types.ph.low:
+      // Error + bag to low
+      statusData.phStatus = 1;
+      break;
+    default:
+      statusData.phStatus = 0;
+      break;
+  }
+  // Temp indicator
+  switch (true) {
+    case data.PT100_real_1 >= types.temp.low && data.PT100_real_1 <= types.temp.high:
+      // Good
+      statusData.tempStatus = 0;
+      break;
+    case data.PT100_real_1 > types.temp.high:
+      // Error + temp to high
+      statusData.tempStatus = 1;
+      break;
+    case data.PT100_real_1 < types.temp.low:
+      // Error + temp to low
+      statusData.tempStatus = 1;
+      break;
+    default:
+      statusData.tempStatus = 0;
+      break;
+  }
+
+  return statusData;
+}
+
 module.exports = router;
