af7fba2ddf7c83eb3e9070213c5c7d91ce0f06e7
Timo Verkroost <timo.verkroost@gmail.com>
2017-06-19 12:59:57 +0200

    Calculate based in range added

--- modules/usage-calculation.js
+++ modules/usage-calculation.js
@@ -75,35 +75,68 @@ function usageCalculation() {
       if (error) {
         throw error;
       }
+      // Get all the seconds
+      function getAll() {
+        let i = 0;
+        const sendItemsCount = 1;
 
-      let i = 0;
-      const sendItemsCount = 1;
-
-      setInterval(() => {
-        if (!output[i + sendItemsCount]) {
-          i = 1;
-        }
-
-        const dataCollection = [];
-
-        for (let x = 1; x <= sendItemsCount; x++) {
-          dataCollection.push(output[x + i]);
-        }
-
-        i += 1;
-        // Unix time in seconds
-        let currentTime = moment(output[i].Date + ' ' + output[i].Time, 'YYYY-MM-DD HH:mm:ss').valueOf() / 1000;
-        let beforeTime  = moment(output[i - 1].Date + ' ' + output[i - 1].Time, 'YYYY-MM-DD HH:mm:ss').valueOf() / 1000;
-        // Adds seconds to
-        Object.keys(output[i]).forEach(function(key) {
-          let valNumberBefore = Number(output[i - 1][key]);
-          if (valNumberBefore === 1) {
-            // Addeds new seconds to object
-            deviceCollection[key].timeON = deviceCollection[key].timeON + (currentTime - beforeTime);
+        setInterval(() => {
+          if (!output[i + sendItemsCount]) {
+            i = 1;
           }
-        });
 
-      }, 1000);
+          const dataCollection = [];
+
+          for (let x = 1; x <= sendItemsCount; x++) {
+            dataCollection.push(output[x + i]);
+          }
+
+          i += 1;
+          // Unix time in seconds
+          let currentTime = moment(output[i].Date + ' ' + output[i].Time, 'DD-MM-YYYY HH:mm:ss').valueOf() / 1000;
+          let beforeTime = moment(output[i - 1].Date + ' ' + output[i - 1].Time, 'DD-MM-YYYY HH:mm:ss').valueOf() / 1000;
+          // Adds seconds to
+          Object.keys(output[i]).forEach(function (key) {
+            let valNumberBefore = Number(output[i - 1][key]);
+            if (valNumberBefore === 1) {
+              // Added new seconds to object
+              deviceCollection[key].timeON = deviceCollection[key].timeON + (currentTime - beforeTime);
+              //console.log(deviceCollection);
+            }
+          });
+
+        }, 1000);
+      }
+      getAll();
+
+      // Get by range
+      function getByrange() {
+        let i;
+        const inputStart = '';
+        const inputRange = 1;
+        const hours = moment.duration(inputRange, 'hours').valueOf() / 1000;
+        const startCount = moment('07-06-2017 10:00:00', 'DD-MM-YYYY HH:mm:ss').valueOf() / 1000;
+        const endCount = startCount + hours;
+
+        for (i = 1; i < output.length; i++) {
+          // Unix time in seconds
+          let currentTime = moment(output[i].Date + ' ' + output[i].Time, 'DD-MM-YYYY HH:mm:ss').valueOf() / 1000;
+          let beforeTime = moment(output[i - 1].Date + ' ' + output[i - 1].Time, 'DD-MM-YYYY HH:mm:ss').valueOf() / 1000;
+          if (currentTime >= startCount && currentTime <= endCount) {
+            // Adds seconds to
+            Object.keys(output[i]).forEach(function (key) {
+              let valNumberBefore = Number(output[i - 1][key]);
+              if (valNumberBefore === 1) {
+                // Added new seconds to object
+                deviceCollection[key].timeON = deviceCollection[key].timeON + (currentTime - beforeTime);
+                console.log(deviceCollection);
+              }
+            });
+          }
+        }
+      }
+      getByrange();
+
     });
   });
 }
