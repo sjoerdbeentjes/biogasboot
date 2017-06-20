b30f015da40e1472202443be165e826464d77bc7
Diego Staphorst <diego-s-@hotmail.com>
2017-06-20 15:14:56 +0200

    New api endpoint month per day

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const parse = require('csv-parse');
 const express = require('express');
 const moment = require('moment');
 const DataPoint = require('../models/dataPoint');
+const StatusPoint = require('../models/statusPoint');
 const router = express.Router();
 const usageCalculation = require('../modules/usage-calculation');
 
@@ -43,42 +44,97 @@ router.get('/range', (req, res, next) => {
       res.send(data);
     });
   } else {
-      res.send('No valid API key');
-    }
+    res.send('No valid API key');
+  }
 });
 
 router.get('/range/average', (req, res, next) => {
   if (req.param('api_key') && req.param('api_key') == process.env.API_KEY) {
     const startDate = moment(Number(req.param('dateStart') * 1000));
     const endDate = moment(Number(req.param('dateEnd') * 1000));
-    DataPoint.aggregate([
-          {
-              $match: {
-                  Date: {
-                    $gte: startDate.toDate(),
-                    $lt: endDate.toDate()
-                  }
-              }
+    DataPoint.aggregate([{
+      $match: {
+          Date: {
+            $gte: startDate.toDate(),
+            $lt: endDate.toDate()
+          }
+        }
+    },
+    {
+      $group: {
+          _id: null,
+          Temp_PT100_1: {
+            $avg: '$Temp_PT100_1'
           },
-          {
-              $group: {
-                  _id: null,
-                  Temp_PT100_1: { $avg: "$Temp_PT100_1" },
-                  Temp_PT100_2: { $avg: "$Temp_PT100_2" },
-                  pH_Value: { $avg: "$pH_Value" },
-                  Bag_Height: { $avg: "$Bag_Height" },
-                  count: {$sum: 1}
-              }
+          Temp_PT100_2: {
+            $avg: '$Temp_PT100_2'
+          },
+          pH_Value: {
+            $avg: '$pH_Value'
+          },
+          Bag_Height: {
+            $avg: '$Bag_Height'
+          },
+          count: {
+            $sum: 1
           }
-      ], function (err, result) {
-          if (err) { console.log(err)
-          } else {
-              res.send(result)
-          }
-      });
-    } else {
-      res.send('No valid API key');
+        }
     }
+    ], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  } else {
+    res.send('No valid API key');
+  }
+});
+
+
+//http://localhost:3000/api/range/monthperday?dateStart=1470002400&dateEnd=1472680800&api_key=CMD17
+router.get('/range/monthperday/', (req, res, next) => {
+  if (req.param('api_key') && req.param('api_key') == process.env.API_KEY) {
+    const startDate = moment(Number(req.param('dateStart') * 1000));
+    const endDate = moment(Number(req.param('dateEnd') * 1000));
+    DataPoint.aggregate([{
+      $match: {
+          Date: {
+            $gte: startDate.toDate(),
+            $lt: endDate.toDate()
+          }
+        }
+    }, {$group: {_id: {
+        year: {$year: '$Date'},
+        month: {$month: '$Date'},
+        day: {$dayOfMonth: '$Date'}
+       }, Temp_PT100_1: {
+            $avg: '$Temp_PT100_1'
+          },
+          Temp_PT100_2: {
+            $avg: '$Temp_PT100_2'
+          },
+          pH_Value: {
+            $avg: '$pH_Value'
+          },
+          Bag_Height: {
+            $avg: '$Bag_Height'
+          },
+          count: {
+            $sum: 1
+          },}
+      }
+    ], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  } else {
+    res.send('No valid API key');
+  }
 });
 
 /* GET home page. */
@@ -115,6 +171,14 @@ router.get('/status/range/:range', (req, res, next) => {
   if (req.param('api_key') && req.param('api_key') == process.env.API_KEY) {
     const range = req.params.range;
     usageCalculation.init(req, res, range);
+  } else {
+    res.send('No valid API key');
+  }
+});
+
+router.get('/status/test', (req, res, next) => {
+  if (req.param('api_key') && req.param('api_key') == process.env.API_KEY) {
+    console.log(StatusPoint);
   } else {
     res.send('No valid API key');
   }
@@ -218,7 +282,7 @@ function tileSatus(data) {
 }
 
 const checkForKey = (req, res) => {
-  console.log('hola')
-}
+  console.log('hola');
+};
 
 module.exports = router;
