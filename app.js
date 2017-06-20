695fd60f04d67f8d12f1207ce29e81e1eb416c25
Diego Staphorst <diego-s-@hotmail.com>
2017-06-20 12:24:22 +0200

    Comment out checking for new data

--- app.js
+++ app.js
@@ -39,9 +39,8 @@ mongoose.connect(process.env.DB_URL);
 
 // Get files/data from FTP
 const FTP = require('./modules/getFTPFiles');
-// console.log(getFTPfiles)
-FTP.checkForNewFilesIn('value');
-FTP.checkForNewFilesIn('status');
+// FTP.checkForNewLocalFiles('value');
+// FTP.checkForNewLocalFiles('status');
 // FTP.checkForNewFilesIn('alarm'); // does not work with current filenames
 
 // websockets
