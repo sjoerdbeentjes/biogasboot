8256df6b9204d0edb580c8c8b38fcab0d30c7c74
Camille Sébastien Niessen <camille.s.niessen@icloud.com>
2017-06-26 15:34:17 +0200

    #62 - Interactive view almost responsive

--- public/js/image-mapping.js
+++ public/js/image-mapping.js
@@ -5,7 +5,36 @@ const config = {
   areaCoords: [],
   vector: false,
   timer: false
-}
+};
+
+const RecalculateImageMap = {
+  init() {
+    if (config.actualMap.newSize) {
+      // Still on this
+    } else {
+      RecalculateImageMap.start();
+    }
+  },
+  start() {
+    config.allAreas = config.actualMap.getElementsByTagName('area');
+    config.vector = document.getElementById('interactive_vector');
+    for (let i = 0; i < config.allAreas.length; i++) {
+      const coords = config.allAreas[i].coords;
+      config.areaCoords.push(coords.replace(/ *, */g, ',').replace(/ +/g, ','));
+    }
+    RecalculateImageMap.resize();
+  },
+  resize() {
+    config.areaCoords.forEach(function (area, i) {
+      config.allAreas[i] = area.split(',').map(RecalculateImageMap.scale).join(',');
+    });
+  },
+  scale(coordinate) {
+    let totalScale = config.vector.width / config.vector.naturalWidth;
+    let result = Math.round(Number(coordinate) * totalScale);
+    console.log(result)
+  }
+};
 
 const ImageMapSetup = {
   init() {
@@ -18,21 +47,3 @@ const ImageMapSetup = {
     }
   }
 };
-
-const RecalculateImageMap = {
-  init() {
-    if(config.actualMap.newSize) {
-    } else {
-      RecalculateImageMap.start();
-    }
-  },
-  start() {
-    config.allAreas = config.actualMap.getElementsByTagName('area');
-    config.vector = document.getElementById('interactive_vector')
-    for(let i = 0; i < config.allAreas.length; i++) {
-      const coords = config.allAreas[i].coords;
-      config.areaCoords.push(coords.replace(/ *, */g, ',').replace(/ +/g, ','))
-    }
-    console.log(config.areaCoords)
-  }
-}
