f986c1d4785d11bdda15d936f6a0a40d4657f985
Timo Verkroost <timo.verkroost@gmail.com>
2017-06-27 14:26:24 +0200

    Redirect home to operator dashboard (realtime)

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Express'});
+  res.redirect('/operator/dashboard');
+  //res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
