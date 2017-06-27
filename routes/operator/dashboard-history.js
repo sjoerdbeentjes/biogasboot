755ba6cf237fcf145628f3bc2ca573c9366f2361
Timo Verkroost <timo.verkroost@gmail.com>
2017-06-27 11:36:36 +0200

    Only show dates that are in DB

--- routes/operator/dashboard-history.js
+++ routes/operator/dashboard-history.js
@@ -20,7 +20,23 @@ router.get('/', (req, res, next) => {
     if (err) {
       console.log(err);
     } else {
-        res.locals.YMdropdown = result;
+        // Get years
+        let years = [];
+        for (let i = 0; i < result.length; i++) {
+          // Only adds to years when not exist to prevent duplicates
+          years.indexOf(result[i]._id.year) === -1 ? years.push(result[i]._id.year): years;
+        }
+        // Get months
+        let months = [];
+        for (let i = 0; i < result.length; i++) {
+          // Only adds to months when not exist to prevent duplicates
+          months.indexOf(result[i]._id.month) === -1 ? months.push(result[i]._id.month): months;
+        }
+        // Init month names
+        const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        res.locals.dorpdownYears = years;
+        res.locals.dorpdownMonths = months;
+        res.locals.dorpdownMonthNames = monthNames;
         res.render('operator/history', {title: 'Operator | Dashboard'});
     }
   });
